26e9d4623d750bcafa99cc4b8d6c5dac0c1fb9ee
AbroShahzeb <khizr77@gmail.com>
2025-01-17 15:06:20 +0500

    Changes

--- src/layout/components/navbar/index.jsx
+++ src/layout/components/navbar/index.jsx
@@ -8,51 +8,53 @@ import { useState } from "react";
 export const Navbar = () => {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
   return (
-    <nav className="bg-primary-white px-6 pb-[34px] pt-[35px] flex items-center gap-2 justify-between md:px-10 md:py-16 xl:max-w-[1111px] xl:mx-auto xl:px-0 sticky top-0 z-30 ">
-      {/* Logo */}
-      <Link to="/">
-        <img src={logoDark} alt="Designo Logo" className="h-[27px]" />
-      </Link>
+    <nav className="bg-primary-white w-full sticky top-0 z-30 ">
+      <div className="bg-primary-white px-6 pb-[34px] pt-[35px] flex items-center gap-2 justify-between md:px-10 md:py-16 xl:max-w-[1111px] xl:mx-auto xl:px-0 ">
+        {/* Logo */}
+        <Link to="/">
+          <img src={logoDark} alt="Designo Logo" className="h-[27px]" />
+        </Link>
 
-      <ul className="hidden md:flex items-center gap-[42px] uppercase text-[14px] leading-[14px] text-secondary-dark-grey tracking-[2px]">
-        <li className="hover:underline transition-all">
-          <NavLink
-            to="/company"
-            className={({ isActive }) => isActive && "underline"}
-          >
-            Our company
-          </NavLink>
-        </li>
-        <li className="hover:underline transition-all">
-          <NavLink
-            to="/locations"
-            className={({ isActive }) => isActive && "underline"}
-          >
-            Locations
-          </NavLink>
-        </li>
-        <li className="hover:underline transition-all">
-          <NavLink
-            to="/contact"
-            className={({ isActive }) => isActive && "underline"}
-          >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
+        <ul className="hidden md:flex items-center gap-[42px] uppercase text-[14px] leading-[14px] text-secondary-dark-grey tracking-[2px]">
+          <li className="hover:underline transition-all">
+            <NavLink
+              to="/company"
+              className={({ isActive }) => isActive && "underline"}
+            >
+              Our company
+            </NavLink>
+          </li>
+          <li className="hover:underline transition-all">
+            <NavLink
+              to="/locations"
+              className={({ isActive }) => isActive && "underline"}
+            >
+              Locations
+            </NavLink>
+          </li>
+          <li className="hover:underline transition-all">
+            <NavLink
+              to="/contact"
+              className={({ isActive }) => isActive && "underline"}
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
 
-      {/* Hamburger Menu */}
-      <button
-        className="md:hidden"
-        onClick={() => setIsMobileNavOpen((prev) => !prev)}
-      >
-        <img
-          src={isMobileNavOpen ? iconClose : iconHamburger}
-          alt={isMobileNavOpen ? "Close Icon" : "Hamburger Icon"}
-        />
-      </button>
+        {/* Hamburger Menu */}
+        <button
+          className="md:hidden"
+          onClick={() => setIsMobileNavOpen((prev) => !prev)}
+        >
+          <img
+            src={isMobileNavOpen ? iconClose : iconHamburger}
+            alt={isMobileNavOpen ? "Close Icon" : "Hamburger Icon"}
+          />
+        </button>
 
-      <MobileNav isOpen={isMobileNavOpen} setIsOpen={setIsMobileNavOpen} />
+        <MobileNav isOpen={isMobileNavOpen} setIsOpen={setIsMobileNavOpen} />
+      </div>
     </nav>
   );
 };
