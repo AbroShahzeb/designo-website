fb16e86995c94dd760f07be6955f65a2947acc7e
AbroShahzeb <khizr77@gmail.com>
2025-01-18 11:48:44 +0500

    Changes

--- src/layout/components/footer/components/contact/index.jsx
+++ src/layout/components/footer/components/contact/index.jsx
@@ -1,6 +1,6 @@
 export const Contact = () => {
   return (
-    <div className="text-center text-primary-white/50 text-base md:text-left md:w-[223px] xl:w-[350px]">
+    <div className="text-center text-primary-white/50 text-base md:text-left md:w-[223px] xl:w-[350px] xl:font-bold">
       <p className="font-bold">Contact Us (Central Office)</p>
       <p>P : +1 253-863-8967</p>
       <p>M : contact@designo.co</p>
